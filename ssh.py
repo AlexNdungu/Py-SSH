387be29d40d4d69613e95b42540f0b48fc27cf1d
AlexNdungu <alexmeta517@gmail.com>
2023-06-24 11:07:38 +0300

    Update ssh file

--- ssh.py
+++ ssh.py
@@ -6,13 +6,14 @@ ssh_client = paramiko.SSHClient()
 
 #Get credentials
 hostname = input("Hostname: ")
+port = int(input("Port: "))
 username = input("Username: ")
 password = getpass.getpass(prompt = 'Enter the password')
 
 #Add to known
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh_client.connect(hostname=hostname, username=username, password=password)
+ssh_client.connect(hostname=hostname, port=port,username=username, password=password)
 stdin, stdout, stderr = ssh_client.exec_command('dir')
 
 #Get the output
